d0aa7789185a8d2ba65364290a163eb9b322bf82
Andrei <ungureanuandrei163303@gmail.com>
2026-02-14 14:43:06 +0200

    Configuratie finala verificata

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './', // Important pentru ca GitHub Pages sa gaseasca fisierele corect
 })
