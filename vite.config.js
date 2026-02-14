533a44403c896e291096755942d084a9ee63ecc9
Andrei <ungureanuandrei163303@gmail.com>
2026-02-14 13:53:25 +0200

    Fix config si pregatire deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-swc'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Această linie repară eroarea 404 din consolă
+  base: './', 
 })
